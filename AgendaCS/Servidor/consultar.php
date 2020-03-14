bdff24a75ce790713e5a741ea040de5e7c9fd196
Fredy Cortés <fred.cor.14@gmail.com>
2020-03-14 12:39:26 -0500

    UploadCS

---
+++ AgendaCS/Servidor/consultar.php
@@ -0,0 +1,13 @@
+<?php
+    require "./index.php";
+
+    $conn = getConnection();
+    if(is_string($conn)) {
+        echo $conn;
+    } else {
+        $stm = $conn->prepare("SELECT * FROM persona");
+        $stm->execute();
+        $data = $stm->fetchAll(PDO::FETCH_ASSOC);
+        $data = array("results" => $data);
+        echo json_encode($data);
+    }
---
+++ AgendaCS/Servidor/registrar.php
@@ -0,0 +1,28 @@
+<?php
+    require "./index.php";
+
+    $conn = getConnection();
+    if(is_string($conn)) {
+        echo $conn;
+    } else {
+        $pers_nombre = $_POST["pers_nombre"];
+        $pers_fijo = $_POST["tele_fijo"];
+        $pers_celular = $_POST["tele_celular"];
+        if($pers_fijo != "" && $pers_celular != "") {
+            $query = "INSERT INTO persona (pers_nombre, pers_fijo, pers_celular) 
+                    VALUES ('{$pers_nombre}', {$pers_fijo}, {$pers_celular})";
+        } else if($pers_fijo != "") {
+            $query = "INSERT INTO persona (pers_nombre, pers_fijo) 
+                    VALUES ('{$pers_nombre}', {$pers_fijo})";
+        } else {
+            $query = "INSERT INTO persona (pers_nombre, pers_celular) 
+                    VALUES ('{$pers_nombre}', {$pers_celular})";
+        }
+        try {
+            $stm = $conn->prepare($query);
+            $stm->execute();
+            echo 1;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
